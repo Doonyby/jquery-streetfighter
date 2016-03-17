bcaad18e3d4eb60c169452fe415c0eed5bc81537
Dan Riding <doonyby@gmail.com>
2016-03-17 08:56:22 -0600

    format

--- js/app.js
+++ js/app.js
@@ -6,48 +6,48 @@ $(document).ready(function() {
 
   .mouseleave(function() {
     $('.ryu-ready').hide();
- 	$('.ryu-still').show();
+    $('.ryu-still').show();
   })
 
   .mousedown(function() {
-  	playHadouken();
+    playHadouken();
     $('.ryu-ready').hide();
     $('.ryu-cool').hide();
-  	$('.ryu-throwing').show();
-	$('.hadouken').finish().show().animate(
-  		{'left': '1020px'},
-  		500,
-  		function() {
-    		$(this).hide();
-    		$(this).css('left', '520px');
-  		});
-  	})
+    $('.ryu-throwing').show();
+    $('.hadouken').finish().show().animate(
+      {'left': '1020px'},
+      500,
+      function() {
+        $(this).hide();
+        $(this).css('left', '520px');
+      });
+  })
   
   .mouseup(function() {
     $('.ryu-throwing').hide();
-  	$('.ryu-ready').hide();
+    $('.ryu-ready').hide();
     $('.ryu-cool').hide();
     $('.ryu-still').show();
   })
 
 
-    $('body').keydown(function(e){
-        if(e.keyCode == 88){
+  $('body').keydown(function(e) {
+    if(e.keyCode == 88) {
       $('.ryu-ready').hide();
- 			$('.ryu-still').hide();
- 			$('.ryu-throwing').hide();
- 			$('.ryu-cool').show(); 
- 		}
-   	});
+      $('.ryu-still').hide();
+      $('.ryu-throwing').hide();
+      $('.ryu-cool').show(); 
+    }
+  });
 
-   	$('body').keyup(function(e){
-        if(e.keyCode == 88){
- 			$('.ryu-cool').hide();
+  $('body').keyup(function(e) {
+    if(e.keyCode == 88) {
+      $('.ryu-cool').hide();
       $('.ryu-ready').hide();
       $('.ryu-throwing').hide(); 
- 			$('.ryu-still').show();
- 		}
-   	});
+      $('.ryu-still').show();
+    }
+  });
 }); 
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
